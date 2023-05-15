b2223db6fa840a820866afb8d25570ae72d67726
m325 <m_kunz14@cs.uni-kl.de>
2023-05-15 16:07:22 +0200

    Added possibility to export locations with only colors or object IDs

--- Source/Sensors/Adapter/ExportAdapter.cpp
+++ Source/Sensors/Adapter/ExportAdapter.cpp
@@ -17,48 +17,82 @@ UExportAdapter::UExportAdapter()
 
 void UExportAdapter::PublishLiDARData(struct FLiDARData LiDARStruct)
 {
-
-  if (this->FilePath.Len() == 0)
+  switch (this->OutputFormat)
   {
-    UE_LOG(LogTemp, Warning, TEXT("ExportAdapter No path for importing given."));
-    return;
-  }
-  else if (FPaths::FileExists(this->FilePath))
-  {
-    TArray<FString> StringArray;
-    //First line is needed for correct Unreal import
-    StringArray.Emplace(TEXT("---,XCoordinate,YCoordinate,ZCoordinate,RColor,GColor,BColor,ObjectID"));
-
-
-    //TODO maybe parallel ?
-    if (LiDARStruct.Locations.Num() != LiDARStruct.ObjectIDs.Num())
+  case EOutputFormat::CSV_simple:
+    if (this->FilePath.Len() == 0)
     {
-      UE_LOG(LogTemp, Warning, TEXT("ExportAdapter Locations and ObjectIDs do not match in size. Locations array has %d many elements and the ObjectIDs array %D"), LiDARStruct.Locations.Num(), LiDARStruct.ObjectIDs.Num());
+      UE_LOG(LogTemp, Warning, TEXT("ExportAdapter No path for importing given."));
       return;
-    }else if (LiDARStruct.Locations.Num() != LiDARStruct.RGBValues.Num()){
-    	UE_LOG(LogTemp, Warning, TEXT("ExportAdapter Locations and RGBValues do not match in size. Locations array has %d many elements and the RGBValues array %D"), LiDARStruct.Locations.Num(), LiDARStruct.RGBValues.Num());
-    	      return;
     }
-    for (int32 i = 0; i < LiDARStruct.Locations.Num(); i++)
+    else if (FPaths::FileExists(this->FilePath))
     {
-      StringArray.Emplace(CreateLine(i, LiDARStruct.Locations[i], LiDARStruct.RGBValues[i], LiDARStruct.ObjectIDs[i]));
-    }
+      TArray<FString> StringArray;
 
+      //TODO maybe parallel ?
+      //FIXME append first line depending on the things that should be published
+      //only Locations
+      if (LiDARStruct.Locations.Num() != LiDARStruct.ObjectIDs.Num() && LiDARStruct.Locations.Num() != LiDARStruct.RGBValues.Num())
+      {
+        StringArray.Emplace(TEXT("---,XCoordinate,YCoordinate,ZCoordinate"));
+        for (int32 i = 0; i < LiDARStruct.Locations.Num(); i++)
+        {
+          StringArray.Emplace(CreateLine(i, LiDARStruct.Locations[i]));
+        }
+      }
+      //Locations with RGB values and ObjectID
+      else if (LiDARStruct.Locations.Num() == LiDARStruct.RGBValues.Num() && LiDARStruct.Locations.Num() == LiDARStruct.ObjectIDs.Num())
+      {
+        //First line is needed for correct Unreal import
+        StringArray.Emplace(TEXT("---,XCoordinate,YCoordinate,ZCoordinate,RColor,GColor,BColor,ObjectID"));
 
-    // We use the LoadFileToString to load the file into
-    if (FFileHelper::SaveStringArrayToFile(StringArray, *FilePath))
-    {
-      UE_LOG(LogTemp, Warning, TEXT("FileManipulation: Succesfully Written to the text file"));
+        for (int32 i = 0; i < LiDARStruct.Locations.Num(); i++)
+        {
+          StringArray.Emplace(CreateLine(i, LiDARStruct.Locations[i], LiDARStruct.RGBValues[i], LiDARStruct.ObjectIDs[i]));
+        }
+      }
+      //Locations with RGB
+      else if(LiDARStruct.Locations.Num() == LiDARStruct.RGBValues.Num() && LiDARStruct.Locations.Num() != LiDARStruct.ObjectIDs.Num())
+      {
+    	  StringArray.Emplace(TEXT("---,XCoordinate,YCoordinate,ZCoordinate,RColor,GColor,BColor"));
+          for (int32 i = 0; i < LiDARStruct.Locations.Num(); i++)
+          {
+            StringArray.Emplace(CreateLine(i, LiDARStruct.Locations[i], LiDARStruct.RGBValues[i]));
+          }
+	}
+      else if (LiDARStruct.Locations.Num() != LiDARStruct.RGBValues.Num() && LiDARStruct.Locations.Num() == LiDARStruct.ObjectIDs.Num()) {
+    	  StringArray.Emplace(TEXT("---,XCoordinate,YCoordinate,ZCoordinate,BColor"));
+          for (int32 i = 0; i < LiDARStruct.Locations.Num(); i++)
+          {
+            StringArray.Emplace(CreateLine(i, LiDARStruct.Locations[i], LiDARStruct.ObjectIDs[i]));
+          }
+	}
+      else
+      {
+        UE_LOG(LogTemp, Warning, TEXT("ExportAdapter Either only output the Locations or RGB values and ObjectsIDs. Locations array has %d many elements and the ObjectIDs array %d and RGB Values %d"), LiDARStruct.Locations.Num(), LiDARStruct.ObjectIDs.Num(), LiDARStruct.RGBValues.Num());
+        return;
+      }
+
+      // We use the LoadFileToString to load the file into
+      if (FFileHelper::SaveStringArrayToFile(StringArray, *FilePath))
+      {
+        UE_LOG(LogTemp, Warning, TEXT("FileManipulation: Succesfully Written to the text file"));
+      }
+      else
+      {
+        UE_LOG(LogTemp, Warning, TEXT("FileManipulation: Failed to write FString to file."));
+      }
     }
     else
     {
-      UE_LOG(LogTemp, Warning, TEXT("FileManipulation: Failed to write FString to file."));
+      UE_LOG(LogTemp, Warning, TEXT("ExportAdapter File does not exist."));
     }
+
+    break;
+  default:
+    break;
   }
-  else
-  {
-    UE_LOG(LogTemp, Warning, TEXT("ExportAdapter File does not exist."));
-  }
+
 
 }
 
@@ -83,3 +117,53 @@ FString UExportAdapter::CreateLine(const int32& LineNumber, const FVector& Locat
 
   return Line;
 }
+
+FString UExportAdapter::CreateLine(const int32& LineNumber, const FVector& Location, const FColor& Color, const FString& Separator)
+{
+  FString Line;
+  Line.Append(FString::FromInt(LineNumber));
+  Line.Append(Separator);
+  Line.Append(FString::SanitizeFloat(Location.X));
+  Line.Append(Separator);
+  Line.Append(FString::SanitizeFloat(Location.Y));
+  Line.Append(Separator);
+  Line.Append(FString::SanitizeFloat(Location.Z));
+  Line.Append(Separator);
+  Line.Append(FString::FromInt(Color.R));
+  Line.Append(Separator);
+  Line.Append(FString::FromInt(Color.G));
+  Line.Append(Separator);
+  Line.Append(FString::FromInt(Color.B));
+
+  return Line;
+}
+
+FString UExportAdapter::CreateLine(const int32& LineNumber, const FVector& Location, const int32& ObjectID, const FString& Separator)
+{
+  FString Line;
+  Line.Append(FString::FromInt(LineNumber));
+  Line.Append(Separator);
+  Line.Append(FString::SanitizeFloat(Location.X));
+  Line.Append(Separator);
+  Line.Append(FString::SanitizeFloat(Location.Y));
+  Line.Append(Separator);
+  Line.Append(FString::SanitizeFloat(Location.Z));
+  Line.Append(Separator);
+  Line.Append(FString::FromInt(ObjectID));
+
+  return Line;
+}
+
+FString UExportAdapter::CreateLine(const int32& LineNumber, const FVector& Location, const FString& Separator)
+{
+  FString Line;
+  Line.Append(FString::FromInt(LineNumber));
+  Line.Append(Separator);
+  Line.Append(FString::SanitizeFloat(Location.X));
+  Line.Append(Separator);
+  Line.Append(FString::SanitizeFloat(Location.Y));
+  Line.Append(Separator);
+  Line.Append(FString::SanitizeFloat(Location.Z));
+  return Line;
+}
+
--- Source/Sensors/Adapter/ExportAdapter.h
+++ Source/Sensors/Adapter/ExportAdapter.h
@@ -6,8 +6,15 @@
 #include "Adapter/AdapterComponent.h"
 #include "ExportAdapter.generated.h"
 
+UENUM(BlueprintType)
+enum class EOutputFormat : uint8
+{
+  CSV_simple UMETA(DisplayName = "CSV"),
+  Undefined UMETA(DisplayName = "Undefined")
+};
+
 /**
- *
+ * This adapter is used to export single sensor recordings (currently only LiDAR) to a csv file in a predefined format.
  */
 UCLASS()
 class SENSORS_API UExportAdapter : public UAdapterComponent
@@ -23,9 +30,17 @@ public:
   UPROPERTY(EditAnywhere, BlueprintReadWrite)
   FString FilePath;
 
+  /**Defines the output format of the data.*/
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  EOutputFormat OutputFormat;
+
   virtual void PublishLiDARData(struct FLiDARData LiDARStruct) override;
 
 private:
+  //FIXME Template?
   //Converts a row to a single FString
   FString CreateLine(const int32& LineNumber, const FVector& Location, const FColor& Color, const int32& ObjectID, const FString& Separator = TEXT(","));
+  FString CreateLine(const int32& LineNumber, const FVector& Location, const FColor& Color, const FString& Separator = TEXT(","));
+  FString CreateLine(const int32& LineNumber, const FVector& Location, const int32& ObjectID, const FString& Separator = TEXT(","));
+  FString CreateLine(const int32& LineNumber, const FVector& Location, const FString& Separator = TEXT(","));
 };
